39b482628f8e080e790d736a70409f6a8a54062c
GDoval <charly_sxxi@hotmail.com>
2018-03-21 09:44:05 -0300

    Signed-off-by: GDoval <charly_sxxi@hotmail.com>

---
+++ Doval.Gaston/Clase_1_Laboratorio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_1_Laboratorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "Esto no es un mensaje";
+            Sello.Imprimir();
+            //Sello.Borrar();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Doval.Gaston/Clase_1_Laboratorio/Sello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_1_Laboratorio
+{
+    class Sello
+    {
+        public static string mensaje;
+        public static void Imprimir()
+        {
+            Console.WriteLine("{0}", Sello.mensaje);
+        }
+        public static void Borrar()
+        {
+            Console.Clear();
+        }
+
+    }
+}
