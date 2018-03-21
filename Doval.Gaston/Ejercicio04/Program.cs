7a67a4c078ce3eaf7d9d91c2e816e539e9864f6f
GDoval <jsarasola@imsp.intermedia>
2018-03-21 18:53:11 -0300

    asd

---
+++ Doval.Gaston/Ejercicio04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04
+{
+    class Program// Te saca los 4 primeros numeros perfectos te saca
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 04";
+            int suma = 0;
+            int asd = 0;
+            for (int i = 10000; i > 1; i--)
+            {
+                suma = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0  &&  i != j)
+                    {
+                        suma = suma + j;
+                    }
+                }
+                if (suma == i)
+                    Console.WriteLine("{0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
