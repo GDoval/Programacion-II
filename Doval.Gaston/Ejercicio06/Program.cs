6c2158bf22b8072c1ef2edb9890c5027bf51fdd8
GDoval <jsarasola@imsp.intermedia>
2018-03-22 18:53:58 -0300

    asd

---
+++ Doval.Gaston/Ejercicio06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 06";
+            int year;
+            Console.Write("Ingresame un año: ");
+            year = int.Parse(Console.ReadLine());
+            if ((year % 4) == 0 || (year % 100 == 0 && year % 400 == 0))
+            {
+                Console.WriteLine("Año bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("Nope");
+            }
+            Console.ReadLine();
+        }
+    }
+}
