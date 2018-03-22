6221e0e7f1008607fd413becef5acca4cd359f85
GDoval <jsarasola@imsp.intermedia>
2018-03-22 18:24:33 -0300

    asd

---
+++ Doval.Gaston/Ejercicio05/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 06";
+            int num;
+            int acum = 0;
+            int acum2 = 0;
+            int cont = 0;
+            int ancla = 2;
+            Console.WriteLine("Ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+            while (cont < num)
+            {
+                for (int i = 1; i < ancla; i ++ )
+                {
+
+                    acum = acum + i;
+                }
+                for (int j = ancla; j <= num; j++)
+                {
+                    if (j != ancla)
+                        acum2 = acum2 + j;
+                }
+                if (acum == acum2)
+                     Console.WriteLine("Tu centro es: {0}", ancla);
+               /* if (true)
+                    Console.WriteLine("{0}  {1}  {2}", ancla, acum, acum2);*/
+                acum = 0;
+                acum2 = 0;
+                ancla++;
+                cont++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
