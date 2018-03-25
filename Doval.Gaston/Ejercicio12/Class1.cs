b81faa7f7859742f77bc7dc25d21da62232b13c3
GDoval <charly_sxxi@hotmail.com>
2018-03-25 16:49:30 -0300

    sad

---
+++ Doval.Gaston/Ejercicio12/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio12
+{
+    class ValidarRespuesta
+    {
+        public static bool ValidaS_N(char c)
+        {
+            bool resp = c.Equals('S');
+            return resp;
+        }
+    }
+}
---
+++ Doval.Gaston/Ejercicio12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 12";
+            bool resp = true;
+            int num, sum = 0;
+            char s;
+            do
+            {
+                Console.Write("Ingrese un numero: ");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                Console.Write("\nQuiere seguir ingresando numeros? S/N: ");
+                s = char.Parse(Console.ReadLine());
+                resp = ValidarRespuesta.ValidaS_N(s);
+            } while (resp);
+            Console.Write("La suma total de todo es: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
