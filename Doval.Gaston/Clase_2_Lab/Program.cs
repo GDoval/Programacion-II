2341f25884f43cf0e0b464ea0c8cd18bcaa9863e
GDoval <charly_sxxi@hotmail.com>
2018-03-28 10:16:09 -0300

    ASDº

---
+++ Doval.Gaston/Clase_2_Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cosa instancia = new Cosa();
+            Console.Write("Constructor por defecto: \n{0}\n\n", instancia.Mostrar());
+
+            DateTime fecha = new DateTime(1988, 03, 14);
+            Cosa otra = new Cosa(190, "Esto no es un string", fecha);
+            Console.Write("Constructor con valores pasados por el usuario: \n{0}\n\n", otra.Mostrar());
+
+            instancia.EstablecerValor(5);
+            instancia.EstablecerValor("Hola");
+            instancia.EstablecerValor(DateTime.Now);
+
+            Console.Write("\nDespues de inicializar: \n{0}", instancia.Mostrar());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Doval.Gaston/ClassLibrary1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public class Cosa
+    {
+        public int entero;
+        public string cadena;
+        public DateTime fecha;
+
+        /// <summary>
+        /// Establece un valor para un atributo de la clase
+        /// </summary>
+        /// <param name="param">Parametro para inicializar el atributo entero de la clase</param>
+        public void EstablecerValor(int param)
+        {
+            this.entero = param;
+        }
+
+
+        /// <summary>
+        /// Establece un valor para un atributo de la clase
+        /// </summary>
+        /// <param name="param">Incializa el atributo de tipo string de la clase</param>
+        public void EstablecerValor(string param)
+        {
+            this.cadena = param;
+        }
+
+
+        /// <summary>
+        /// Establece un valor para un atributo de la clase
+        /// </summary>
+        /// <param name="param">Incializa la fecha de la clase</param>
+        public void EstablecerValor(DateTime param)
+        {
+            this.fecha = param;
+        }
+
+        /// <summary>
+        /// Muestra los valores actuales de los atributos de la instancia de la clase 
+        /// </summary>
+        /// <returns></returns>
+        public string Mostrar()
+        {
+            string retorno = "";
+            retorno += this.entero + "\n" + this.cadena + "\n" + this.fecha;
+            return retorno;
+        }
+        public Cosa()
+        {
+            this.entero = 10;
+            this.cadena = "Sin valor";
+            this.fecha = DateTime.Now;
+        }
+
+        public Cosa(int num, string cadena, DateTime fecha)
+        {
+            this.entero = num;
+            this.cadena = cadena;
+            this.fecha = fecha;
+        }
+
+
+    }
+}
