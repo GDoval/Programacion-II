4069b60fc3c7bfc836769bee57dc585df69a1e11
GDoval <charly_sxxi@hotmail.com>
2018-04-04 18:10:50 -0300

    asda

---
+++ Doval.Gaston/Esto no es una prueba de conversores/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esto_no_es_una_prueba_de_conversores
+{
+    class Cosas
+    {
+        int algo;
+        double halgo;
+        public Cosas(int num, double aj)
+        {
+            this.algo = num;
+            this.halgo = aj;
+        }
+
+        public static implicit operator int(Cosas algo)
+        {
+            return algo.algo;
+        }
+
+        public static implicit operator double(Cosas asd)
+        {
+            return asd.halgo;
+        }
+    }
+}
---
+++ Doval.Gaston/Esto no es una prueba de conversores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esto_no_es_una_prueba_de_conversores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cosas jota = new Cosas(10, 10.05);
+            int pepe = jota;
+            double romina = jota;
+            Console.WriteLine("{0}  {1}", pepe, romina);
+            Console.ReadLine();
+        }
+    }
+}
