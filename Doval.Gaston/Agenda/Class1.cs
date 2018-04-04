7557ac4d755b7fbaed8c7f98aadfd90f28a6dbe4
GDoval <charly_sxxi@hotmail.com>
2018-04-04 19:37:13 -0300

    asdad

---
+++ Doval.Gaston/Agenda/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Agenda
+    {
+        private Persona[] array_personas;
+        private Persona persona;
+        private int cant;
+
+        public Agenda(string nombre, string mail, int dni, int tel)
+        {
+            persona.SetNom(nombre);
+            persona.SetTel(tel);
+            persona.SetMail(mail);
+            persona.SetDni(dni);
+        }
+
+        public Agenda() :this("Sin Ingresar", "Sin ingresar", 0, 0)
+        {   
+        }
+    }
+
+
+
+    class Persona
+    {
+        private string nombre;
+        private int telefono;
+        private int dni;
+        private string mail;
+
+        public void SetNom(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void SetMail(string mail)
+        {
+            this.mail = mail;
+        }
+
+        public void SetDni(int dni)
+        {
+            this.dni = dni;
+        }
+
+        public void SetTel(int tel)
+        {
+            this.telefono = tel;
+        }
+
+    }
+}
