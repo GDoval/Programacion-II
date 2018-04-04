966341705658b9405dc5ace9427b1fe8b90fe3e3
GDoval <charly_sxxi@hotmail.com>
2018-04-04 10:29:13 -0300

    df

---
+++ Doval.Gaston/Libreria_Clase_Arrays/Paleta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria_Clase_Arrays
+{
+    public class Paleta
+    {
+        private Tempera[] colores;
+        private int CantidadMaximaColores;
+
+        private Paleta() :this(5)
+        {
+        }
+
+        private Paleta(int num)
+        {
+            this.CantidadMaximaColores = num;
+            this.colores = new Tempera[this.CantidadMaximaColores];
+       }
+
+        public static implicit operator Paleta(int num)
+        {
+            Paleta pale = new Paleta(num);
+            return pale;
+        }
+
+        private string Mostrar()
+        {
+            string asd  = "";
+            for (int i = 0; i < this.colores.GetLength(0) ; i++)
+            {
+                asd += this.colores[i].Mostrar(colores[i]);
+            }
+            return asd;
+        }
+
+        public static explicit operator string(Paleta pale)
+        {
+            return pale.Mostrar();
+        }
+    }
+}
---
+++ Doval.Gaston/Libreria_Paleta/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria_Clase_Arrays;
+
+namespace Libreria_Paleta
+{
+    public class Paleta
+    {
+        private Tempera[] colores;
+        private int CantidadMaximaColores;
+
+        private Paleta() :this(5)
+        {
+        }
+
+        private Paleta(int num)
+        {
+            this.CantidadMaximaColores = num;
+            this.colores = new Tempera[this.CantidadMaximaColores];
+       }
+
+        public static implicit operator Tempera(int num)
+        {
+
+        }
+    }
+}
