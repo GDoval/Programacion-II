b9c3127c3cc2ba3a8edfb5158b338f5922deac17
GDoval <charly_sxxi@hotmail.com>
2018-04-06 16:34:34 -0300

    asda

---
+++ Doval.Gaston/Practica_Parcial/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Practica_Parcial
+{
+    partial class numero1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(57, 73);
+            this.num1.Multiline = true;
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(189, 68);
+            this.num1.TabIndex = 0;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(500, 74);
+            this.num2.Multiline = true;
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(197, 67);
+            this.num2.TabIndex = 1;
+            // 
+            // combo
+            // 
+            this.combo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.combo.FormattingEnabled = true;
+            this.combo.Items.AddRange(new object[] {
+            "+",
+            "*",
+            "/",
+            "-"});
+            this.combo.Location = new System.Drawing.Point(317, 73);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(121, 63);
+            this.combo.TabIndex = 2;
+            this.combo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Operar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(207, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 61);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Limpiar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(372, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(525, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 60);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // numero1
+            // 
+            this.ClientSize = new System.Drawing.Size(761, 274);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.combo);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "numero1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Doval.Gaston/Practica_Parcial/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Parcial
+{
+    public partial class numero1 : Form
+    {
+        public numero1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.combo.Items.Add("+");
+            this.combo.Items.Add("/");
+            this.combo.Items.Add("*");
+            this.combo.Items.Add("-");
+        }
+
+
+    }
+}
