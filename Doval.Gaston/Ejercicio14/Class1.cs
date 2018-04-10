b4b0cf4a885b8856980f990e768cef7f16479ed4
GDoval <charly_sxxi@hotmail.com>
2018-04-10 14:53:24 -0300

    Ejercicio 14

---
+++ Doval.Gaston/Ejercicio14/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double num)
+        {
+            return num * num;
+        }
+
+        public static double CalcularTriangulo(double base1, double altura)
+        {
+            return 0.5 * (base1 * altura); 
+        }
+
+        public static double CalcularCirculo(double radio)
+        {
+            double pi = Math.PI;
+            return pi * (radio * radio);
+        }
+    }
+}
---
+++ Doval.Gaston/Ejercicio14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 14";
+            double uno, dos;
+            int opcion;
+            Console.Write("Te calculo el area de algo te calculo\n");
+            Console.WriteLine("**********************************");
+            Console.Write("Elegi:\n1) Cuadrado\n2)Triangulo\n3)Circulo\n ");
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Ingresa el lado: ");
+                    uno = double.Parse(Console.ReadLine());
+                    Console.Write("\nEl area es: {0}", CalculoDeArea.CalcularCuadrado(uno));
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.Write("Ingresa la base: ");
+                    uno = double.Parse(Console.ReadLine());
+                    Console.Write("\nIngresa la altura: ");
+                    dos = double.Parse(Console.ReadLine());
+                    Console.Write("\nEl area es: {0}", CalculoDeArea.CalcularTriangulo(uno, dos));
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.Write("Ingresa el radio: ");
+                    uno = double.Parse(Console.ReadLine());
+                    Console.Write("\nEl area es: {0}", CalculoDeArea.CalcularCirculo(uno));
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
