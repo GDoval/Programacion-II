ed568f925061426443d670c10fbc752c194e71ac
GDoval <charly_sxxi@hotmail.com>
2018-04-10 14:22:40 -0300

    Ejercicio 13

---
+++ Doval.Gaston/Ejercicio13/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Conversor
+    {
+        public static string DecimalBinario(int num)
+        {
+            string resp = "";
+            while (num > 1)
+            {
+                int resto = num % 2;
+                resp = Convert.ToString(resto) + resp;
+                num /= 2;
+            }
+            resp = Convert.ToString(num) + resp;
+            return resp;
+        }
+
+        public static int BinarioDecimal(string num)
+        {
+            int resp = Convert.ToInt32(num, 2);
+            return resp;
+
+        }
+    }
+}
---
+++ Doval.Gaston/Ejercicio13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 13";
+            int num = 65;
+            string a = "1101";
+            string asd = Conversor.DecimalBinario(num);
+            Console.Write("{0}", asd);
+            Console.ReadLine();
+            num = Conversor.BinarioDecimal(a);
+            Console.Write("{0}", num);
+            Console.ReadLine();
+        }
+    }
+}
